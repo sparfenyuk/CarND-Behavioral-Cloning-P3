a3364555ead221bff3563923a698fc6a7188c0ef
Sergey Parfenyuk <Sergey.Parfenyuk@gmail.com>
2017-08-02 22:54:35 +0300

    Switch to LeNet architecture

--- train.py
+++ train.py
@@ -24,13 +24,22 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x/255. - .5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
