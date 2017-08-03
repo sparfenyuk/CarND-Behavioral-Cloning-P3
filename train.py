5fe0bee3c78041e28b88ce9022c4ba6722fda47c
Sergey Parfenyuk <Sergey.Parfenyuk@gmail.com>
2017-08-03 12:14:35 +0300

    Set parameters from command line. Improve Lenet NN.

--- train.py
+++ train.py
@@ -1,8 +1,20 @@
 import csv
 import cv2
 import numpy as np
+import argparse
 
-DATA_FOLDER = './data2'
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--data", type=str, default='./data',
+	help="(optional) folder where data images are located")
+ap.add_argument("-l", "--load-model", type=int, default=-1,
+	help="(optional) whether or not pre-trained model should be loaded")
+ap.add_argument("-i", "--offset", type=int, default=1,
+	help="(optional) read images with offset, convenient for debugging")
+ap.add_argument("-e", "--epochs", type=int, default=2,
+	help="(optional) read images with offset, convenient for debugging")
+args = vars(ap.parse_args())
+
+DATA_FOLDER = args['data']
 lines = []
 with open(DATA_FOLDER + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -14,10 +26,10 @@ measurements = []
 
 print("Preparing data...")
 
-selection_step = 10 # for DEBUG purposes
+offset = args["offset"] # for DEBUG purposes
 
 """Load images from DATA_FOLDER"""
-for line in lines[::selection_step]:
+for line in lines[::offset]:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = DATA_FOLDER + '/IMG/' + filename
@@ -44,11 +56,12 @@ run_augmentation()
 """Setup training data"""
 X_train = np.array(images)
 y_train = np.array(measurements)
+EPOCHS = args["epochs"]
 
 print("Done. {} images loaded".format(X_train.shape[0]))
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -58,14 +71,16 @@ model = Sequential()
 model.add(Lambda(lambda x: x/255. - .5, input_shape=(160, 320, 3)))
 model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
+model.add(Dropout(0.5))
 model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
+model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(120, activation='relu'))
+model.add(Dense(84, activation='relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=.2, shuffle=True, nb_epoch=EPOCHS)
 
 model.save('model.h5')
